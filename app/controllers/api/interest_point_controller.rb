afcc5978e1c92f67910a23daa885470201d59851
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2019-01-19 20:51:50 +0100

    added a Procfile with release instruction, and renamed correctly the constant in code.

--- app/controllers/api/interest_point_controller.rb
+++ app/controllers/api/interest_point_controller.rb
@@ -1,11 +1,11 @@
 class Api::InterestPointController < ApplicationController
-  MaxDistanceToDisplay = 200 # in km
+  MAX_DISTANCE_TO_DISPLAY = 200 # in km
 
   def find_by
     latitude = params['lat']
     longitude = params['lng']
 
-    ret = InterestPoint.within(MaxDistanceToDisplay, :origin => [latitude, longitude])
+    ret = InterestPoint.within(MAX_DISTANCE_TO_DISPLAY, :origin => [latitude, longitude])
     render :json => ret
   end
 end
