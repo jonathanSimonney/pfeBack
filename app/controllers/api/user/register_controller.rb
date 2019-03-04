5581d25af3cb6c84a81dda45d6b6a540d56f2b33
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2019-03-04 18:13:59 +0100

    user creation through API functioning.

--- app/controllers/api/user/register_controller.rb
+++ app/controllers/api/user/register_controller.rb
@@ -1,3 +1,4 @@
 class Api::User::RegisterController < Devise::RegistrationsController
+  protect_from_forgery prepend: true
   respond_to :json
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', action: :index, controller: 'welcome'
 
   namespace :api do
     namespace :user do
-      post '/create', to: 'register#new'
+      devise_scope :user do
+        post '/create', to: 'register#create'
+      end
+      # post '/create', to: 'register#create'
     end
     namespace :map do
       namespace :interest_points do
