1d8797a0f461a22232cc0d1eea862dc959c3eaf8
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2019-03-06 16:47:08 +0100

    all data needed for effective return on signup put in controller.

--- app/controllers/api/user/register_controller.rb
+++ app/controllers/api/user/register_controller.rb
@@ -1,4 +1,17 @@
 class Api::User::RegisterController < Devise::RegistrationsController
   protect_from_forgery prepend: true
   respond_to :json
+  def create
+    super do |resource|
+      if resource.persisted?
+        puts "persisted resource" # a user was created
+      else
+        puts "not persisted resource" # no user was created (be it because of duplicate field OR password not long enough)
+
+        puts resource.errors.full_messages
+        puts resource.errors.full_messages.is_a?(Array)
+        puts "a".is_a?(Array)
+      end
+    end
+  end
 end
