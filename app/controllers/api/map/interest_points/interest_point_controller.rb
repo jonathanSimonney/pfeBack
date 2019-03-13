5f198202c7ab9ba62133aff5906e4ae3aecaf661
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2019-03-13 16:29:31 +0100

    added a time constraint for position sharing.

--- app/controllers/api/map/interest_points/interest_point_controller.rb
+++ app/controllers/api/map/interest_points/interest_point_controller.rb
@@ -1,12 +1,17 @@
 class Api::Map::InterestPoints::InterestPointController < ApplicationController
   MAX_DISTANCE_TO_DISPLAY = 200 # in km
+  MAX_TIME_SINCE_LAST_SHARE = 24.hours
 
   def find_by
+    now = Time.now
+
     latitude = params['lat'].convert_to_decimal
     longitude = params['lng'].convert_to_decimal
 
     interest_points = InterestPoint.within(MAX_DISTANCE_TO_DISPLAY, :origin => [latitude, longitude])
     positions = Position.within(MAX_DISTANCE_TO_DISPLAY, :origin => [latitude, longitude])
-    render :json => {:interest_points => interest_points, :positions => positions}
+
+    recent_positions = positions.select {|position| position.shared_at > now - MAX_TIME_SINCE_LAST_SHARE }
+    render :json => {:interest_points => interest_points, :positions => recent_positions}
   end
 end
