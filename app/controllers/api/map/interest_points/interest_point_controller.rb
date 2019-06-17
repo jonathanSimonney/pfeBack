0f495c0202cdd843a60a9ca429f19cfc2aac9593
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2019-06-17 22:41:13 +0200

    list of opinions is now sent back with the opinions linked.

--- app/controllers/api/map/interest_points/interest_point_controller.rb
+++ app/controllers/api/map/interest_points/interest_point_controller.rb
@@ -14,6 +14,15 @@ class Api::Map::InterestPoints::InterestPointController < ApplicationController
     positions = Position.within(MAX_DISTANCE_TO_DISPLAY, origin: [latitude, longitude])
 
     recent_positions = positions.select { |position| position.shared_at > now - MAX_TIME_SINCE_LAST_SHARE }
-    render json: { interest_points: interest_points, positions: recent_positions }
+
+    ret = []
+
+    for interest_point in interest_points
+      opinion_list = InterestPointOpinion.where(interest_point_id: interest_point["id"])
+      average_grade = opinion_list.inject(0) { |sum, el| sum + el['grade'] }.to_f / opinion_list.size
+      ret.push({interest_point: interest_point, opinions: opinion_list, average_grade: average_grade})
+    end
+
+    render json: { interest_points: ret, positions: recent_positions }
   end
 end
